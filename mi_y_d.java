164c16c263b056fa6a039152983f7a5f337bc601
parthm2310 <86843036+parthm2310@users.noreply.github.com>
2021-09-12 21:45:55 +0530

    Add files via upload

---
+++ mi_y_d.java
@@ -0,0 +1,15 @@
+package main;
+import java.util.*;
+
+public class mi_y_d {
+
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        System.out.println("Enter number of minutes :");
+        int min = a.nextInt();
+        int day,year,y = 525600;
+        day = (min/60/24)%365;
+        year = min/y;
+        System.out.println(min+" mins is approximately "+day+" days and "+year+" years.");
+    }
+}
