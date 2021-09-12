dfe670f02add693b9c9596107b91080479a65cc9
parthm2310 <86843036+parthm2310@users.noreply.github.com>
2021-09-12 21:43:12 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
-package com.company;
+package main;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("hello world!!!");
+        Scanner a = new Scanner(System.in);
+        System.out.println("Enter number of minutes :");
+        int min = a.nextInt();
+        int day,year,y = 525600;
+        day = (min/60/24)%365;
+        year = min/y;
+        System.out.println(min+" mins is approximately "+day+" days and "+year+" years.");
     }
 }
-
